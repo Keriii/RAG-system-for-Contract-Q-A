9b33802a6ba02e599f1c1f02d962dd8d20514686
kerod sisay <kerod53@gmail.com>
2024-02-26 04:38:01 +0300

    backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,48 @@
-from fastapi import FastAPI
+from fastapi import File, UploadFile, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from rag_pipeline import *
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+class Query(BaseModel):
+    objective: str
+
+def load_env_vars():
+    load_dotenv()
+    return os.environ["OPENAI_API_KEY"]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = f'/home/kerod/Desktop/week_11/RAG-system-for-Contract-Q-A/backend/file/{file.filename}'
+    with open(file_path, "wb+") as file_object:
+        file_object.write(await file.read())
+    return {"filename": file.filename}
+
+@app.post("/query")
+async def process_query(query: Query):
+    OPENAI_KEY = load_env_vars()
+    file_path = f'/home/kerod/Desktop/week_11/RAG-system-for-Contract-Q-A/backend/file/RobinsonAdvisory.pdf'
+    loader = PyPDFLoader(file_path)
+    data = data_loader(loader)
+    chunk = text_splitter(data)
+    vectorstore = create_vectoreStore(chunk)
+    retriever = create_retriever(vectorstore)
+    chain = create_chian(retriever)
+    answer = response(chain, query.objective)
+    return {"Answer": answer}
---
+++ backend/rag_pipeline.py
@@ -0,0 +1,106 @@
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Weaviate
+from langchain_openai import ChatOpenAI
+
+import weaviate
+from dotenv import load_dotenv,find_dotenv
+from weaviate.embedded import EmbeddedOptions
+from langchain_community.document_loaders import PyPDFLoader
+
+from langchain_core.runnables import (
+    RunnableParallel,
+    RunnablePassthrough
+)
+from langchain.schema.output_parser import StrOutputParser
+from datasets import Dataset
+from ragas import evaluate
+from ragas.metrics import (
+    faithfulness,
+    answer_relevancy,
+    context_recall,
+    context_precision,
+)
+
+import os
+
+load_dotenv()
+
+OPENAI_KEY=os.environ["OPENAI_API_KEY"]
+
+file = '/home/kerod/Desktop/week_11/RAG-system-for-Contract-Q-A/data/Robinson Advisory.docx'
+
+def data_loader(loaders):
+    
+    contract = loaders.load()
+
+    return contract
+
+def text_splitter(doc):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunk  = text_splitter.split_documents(doc)
+
+    return chunk
+
+
+def create_vectoreStore(doc):
+    client = weaviate.Client(embedded_options=EmbeddedOptions)
+    vectorStore =  Weaviate.from_documents(client =  client,
+                                      documents= doc,
+                                      embedding=OpenAIEmbeddings(),
+                                      by_text = False)
+    
+    return vectorStore
+    
+def create_retriever(vectorstore):
+    retriever = vectorstore.as_retriever()
+
+    return retriever
+
+def create_chian(retriever):
+        # Define LLM
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+    # Define prompt template
+    prompt_template = """You are an assistant for question-answering tasks. 
+    Use the following pieces of retrieved context to answer the question. 
+    If you don't know the answer, just say that you don't know. 
+    Question: {question} 
+    Context: {context} 
+    Answer:
+    """
+
+    prompt = ChatPromptTemplate.from_template(prompt_template)
+
+
+    retrieval = RunnableParallel(
+        {"context": retriever,  "question": RunnablePassthrough()} 
+    )
+
+    chain = retrieval | prompt | llm | StrOutputParser()
+
+    return chain
+
+def response(chain,query):
+
+    return chain.invoke(query)
+
+
+
+def main():
+    loader = PyPDFLoader('/home/kerod/Desktop/week_11/RAG-system-for-Contract-Q-A/data/RobinsonAdvisory.pdf')
+    data = data_loader(loader)
+    chunk = text_splitter(data)
+    vectorStore = create_vectoreStore(chunk)
+    retriever = create_retriever(vectorStore)
+    chain = create_chian(retriever)
+    query = input("Enter query")
+    answer = response(chain,query)
+
+    print("Response: " + answer)
+
+if __name__ == "__main__":
+    main()
